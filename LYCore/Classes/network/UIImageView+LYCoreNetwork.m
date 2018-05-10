6c1c9e85efb1e82677e77b238adc47ddf861ee19
Luo Yu <luoyu@luoyu.space>
2018-05-10 16:54:08 +0800

    [Modify] image cache enable for image view web setter

--- LYCore/Classes/network/UIImageView+LYCoreNetwork.m
+++ LYCore/Classes/network/UIImageView+LYCoreNetwork.m
@@ -8,18 +8,45 @@
 
 #import "UIImageView+LYCoreNetwork.h"
 #import <AFNetworking/UIImageView+AFNetworking.h>
-
+#import <AFNetworking/AFAutoPurgingImageCache.h>
+#import <AFNetworking/AFImageDownloader.h>
 
 @implementation UIImageView (LYCoreNetwork)
 
 - (void)setImageWithURLString:(NSString *)URLString andPlaceholderNamed:(NSString *)imageName {
 	
+	// SET PLACEHOLDER IMAGE AT FIRST
+	self.image = [UIImage imageNamed:imageName];
+	
 	if (URLString == nil || ![URLString isKindOfClass:[NSString class]] || [URLString isEqualToString:@""]) {
-		self.image = [UIImage imageNamed:imageName];
+		// URL STRING NOT VALID
 		return;
 	}
 	
-	[self setImageWithURL:[NSURL URLWithString:URLString] placeholderImage:[UIImage imageNamed:imageName]];
+	NSString *imgIdentifier = [URLString lastPathComponent];
+	AFImageDownloader *imgdl = [AFImageDownloader defaultInstance];
+	
+	UIImage *memCached = [imgdl.imageCache imageforRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:URLString]] withAdditionalIdentifier:imgIdentifier];
+	
+	if (!memCached) {
+		// NO MEMORY IMAGE CACHE WAS FOUND
+		
+		// SEARCH FOR HDD IMAGE CACHE
+		NSURLCache *imgURLCache = imgdl.sessionManager.session.configuration.URLCache;
+		NSCachedURLResponse *cachedResp = [imgURLCache cachedResponseForRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:URLString]]];
+		
+		if (cachedResp.data) {
+			// FOUND HDD CACHE
+			self.image = [UIImage imageWithData:cachedResp.data];
+		} else {
+			// NOTHING WAS FOUND
+			// REQUEST FROM WEB
+			[self setImageWithURL:[NSURL URLWithString:URLString] placeholderImage:[UIImage imageNamed:imageName]];
+		}
+		
+	} else {
+		self.image = memCached;
+	}
 }
 
 - (void)setImageWithURLString:(NSString *)URLString andPlaceholderNamed:(NSString *)imageName inBundle:(NSBundle *)bundle {
