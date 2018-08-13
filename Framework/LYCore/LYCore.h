7f4aafc88b5cb43f5961642f08d182b9ba841ca8
Luo Yu <luoyu@luoyu.space>
2018-08-13 11:31:05 +0800

    [Add] framworking project

---
+++ Framework/LYCore/LYCore.h
@@ -0,0 +1,19 @@
+//
+//  LYCore.h
+//  LYCore
+//
+//  Created by Luo Yu on 2018/8/13.
+//  Copyright © 2018 Luo Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LYCore.
+FOUNDATION_EXPORT double LYCoreVersionNumber;
+
+//! Project version string for LYCore.
+FOUNDATION_EXPORT const unsigned char LYCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LYCore/PublicHeader.h>
+
+
